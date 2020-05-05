9c61d02fb0573bc11009f6d083b2c547433b3ead
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2020-05-05 23:13:53 +0200

    working

--- labs/01-Provision_the_local_DB/MS_SQLServer/scripts/entrypoint.sh
+++ labs/01-Provision_the_local_DB/MS_SQLServer/scripts/entrypoint.sh
@@ -8,6 +8,8 @@ echo importing data will start in $wait_time...
 sleep $wait_time
 echo importing data...
 
+#sudo /opt/mssql/bin/mssql-conf set sqlagent.enabled true
+
 /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -i /initdb/initdb.sql
 /opt/mssql-tools/bin/bcp dbo.DatiRegioniStaging in /dati-regioni.csv -S localhost -U sa -P $SA_PASSWORD -d $DB_NAME -k -c -t ","
 /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -i /initdb/copy_staging.sql
