6330170e10b8aa0be5722e36329bae68ae74c880
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2020-05-06 09:15:05 +0200

    fix

---
+++ labs/03-Provision_Kafka/AfterInit/after-init.sh
@@ -0,0 +1,3 @@
+#sleep 90 && wget --post-data "$(cat /register-sqlserver.json)" --header "Accept:application/json"  --header "Content-Type:application/json" http://connect:8083/connectors/
+sleep 90 && curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" http://connect:8083/connectors/ -d @register-sqlserver.json
+#curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" http://connect:8083/connectors/ -d @register-bigquery.json
